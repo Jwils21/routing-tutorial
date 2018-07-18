8f991a41b1b26cd08ff3558df0872b5dac3aa63b
Jonathan Wilson <jon.wilsond@gmail.com>
2018-07-18 15:33:24 -0400

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NumberReaderComponent } from './number-reader/number-reader.component';
+
+
+const routes: Routes = [
+ {path: '', redirectTo: 'login', pathMatch: 'full'},
+ {path: 'home', component: HomeComponent},
+ {path: 'about', component: AboutComponent},
+ {path: 'numreader/:id', component: NumberReaderComponent },
+ {path: '**', component: HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+ imports: [RouterModule.forRoot(routes)],
+ exports: [RouterModule]
 })
 export class AppRoutingModule { }
